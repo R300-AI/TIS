c61cde036cbd8cd7a60b6398dc32882a018587d7
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-10-15 11:30:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from TIS import TIS
-import numpy as np
 import cv2
 
-cap1 = TIS.VideoCapture(0)
-cap2 = TIS.VideoCapture(1)
+cap = TIS.VideoCapture(0)  #Indexing devices by order.
 while True:
-  ret1, frame1 = cap1.read()
-  ret2, frame2 = cap2.read()
-  if ret1 and ret2:
-    frame_merged = np.hstack((frame1, frame2))
-    frame_merged = cv2.resize(frame_merged, (900, 256), interpolation=cv2.INTER_AREA)
-    
-    cv2.imshow('Demo', frame_merged)
+  ret, frame = cap1.read()
+  if ret:
+    cv2.imshow('Demo', frame)
     if cv2.waitKey(1) == ord('q'):
       break
