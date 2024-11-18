8c94c909b36f131fe2647da0cdd77e68ac3ae532
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-11-18 08:10:45 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 from TIS import TIS
 import cv2
 
-cap = TIS.VideoCapture(0)  #Indexing devices by order.
+cap = TIS.VideoCapture(0)
 while True:
-  ret, frame = cap1.read()
+  ret, frame = cap.read()
   if ret:
     cv2.imshow('Demo', frame)
     if cv2.waitKey(1) == ord('q'):
